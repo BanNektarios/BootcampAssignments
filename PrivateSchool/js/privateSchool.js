4f42608932984186042d8d407141594d892eb2f2
Nektarios <alis18668002@uniwa.gr>
2020-11-10 21:01:42 +0200

    Added  Student Class and updated Courses Class

--- PrivateSchool/js/privateSchool.js
+++ PrivateSchool/js/privateSchool.js
@@ -6,8 +6,30 @@ class Course {
         this.startDate =startDate;
         this.endDate= endDate;
 
-        this.newCourse = function () {
-            return [`${this.title}`, `${this.stream}`, `${this.type}`, `${this.startDate}`, `${this.endDate}`]
-        }
+        this.newCourse = new Map();
+        newCourse.set ('Course info :',{
+            'Title': `${this.title}`,
+            'Stream': `${this.stream}`,
+            'Type': `${this.type}`,
+            'Start Date': `${this.startDate}`,
+            'End Date': `${this.endDate}`
+        });
+    }
+}
+
+class Student {
+    constructor(firstName, lastName, dateOfBirth, tuitionFees) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.tuitionFees = tuitionFees;
+
+        this.newStudent = new Map();
+        newStudent.set ('Student info :',{
+            'First Name': `${this.firstName}`,
+            'Last Name': `${this.lastName}`,
+            'Date of Birth': `${this.dateOfBirth}`,
+            'Tuition Fees': `${this.tuitionFees}`            
+        });
     }
 }
