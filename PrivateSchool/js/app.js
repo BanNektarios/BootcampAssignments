a2fdbc6a8cc4b249e5a423feefe5d0a12a298968
Nektarios <alis18668002@uniwa.gr>
2020-11-17 15:53:10 +0200

    changed all Ids to class in html Had to change/delete variables. Made another way submit forms

--- PrivateSchool/js/app.js
+++ PrivateSchool/js/app.js
@@ -1,43 +1,43 @@
 // ------------> VARIABLES <------------
     // TRAINER
-const trainerFirstName = document.getElementById("trainerFirstName");
-const trainerLastName = document.getElementById("trainerLastName");
-const trainerSubject = document.getElementById("trainerSubject");
+const trainerFirstName = document.getElementsByClassName("trainerFirstName");
+const trainerLastName = document.getElementsByClassName("trainerLastName");
+const trainerSubject = document.getElementsByClassName("trainerSubject");
 let trainerId = 1;
 let trainerArray = [];
 
     // STUDENT
-const studentFirstName = document.getElementById("studentFirstName");
-const studentLastName = document.getElementById("studentLastName");
-const dateOfBirth = document.getElementById("dateOfBirth");
-const tuitionFees = document.getElementById("tuitionFees");
+const studentFirstName = document.getElementsByClassName("studentFirstName");
+const studentLastName = document.getElementsByClassName("studentLastName");
+const dateOfBirth = document.getElementsByClassName("dateOfBirth");
+const tuitionFees = document.getElementsByClassName("tuitionFees");
 let studentId = 1;
 let studentArray = [];
 
 
     // COURSE
-const courseTitle = document.getElementById("courseTitle");
-const courseStream = document.getElementById("courseStream");
-const courseType = document.getElementById("courseType");
-const start_date = document.getElementById("start_date");
-const end_date = document.getElementById("end_date");
+// const courseTitle = document.getElementsByClassName("courseTitle");
+// const courseStream = document.getElementsByClassName("courseStream");
+// const courseType = document.getElementsByClassName("courseType");
+// const start_date = document.getElementsByClassName("start_date");
+// const end_date = document.getElementsByClassName("end_date");
 let courseId = 1;
 let courseArray = [];
 
     // ASSIGNMENT
-const assignmentTitle = document.getElementById("assignmentTitle");
-const assignmentDescription = document.getElementById("assignmentDescription");
-const assignmentSubDate = document.getElementById("assignmentSubDate");
-const assignmentOralMark = document.getElementById("assignmentOralMark");
-const assignmentTotalMark = document.getElementById("assignmentTotalMark");
+const assignmentTitle = document.getElementsByClassName("assignmentTitle");
+const assignmentDescription = document.getElementsByClassName("assignmentDescription");
+const assignmentSubDate = document.getElementsByClassName("assignmentSubDate");
+const assignmentOralMark = document.getElementsByClassName("assignmentOralMark");
+const assignmentTotalMark = document.getElementsByClassName("assignmentTotalMark");
 let assignmentId = 1;
 let assignmentArray = [];
 
     // ADD BUTTONS
-const addTrainerBtn = document.getElementById("addTrainer");
-const addCourseBtn = document.getElementById("addCourse");
-const addStudentBtn = document.getElementById("addStudent");
-const addAssignmentBtn = document.getElementById("addAssignment");
+// const addTrainerBtn = document.getElementsByClassName("addTrainer");
+// const addCourseBtn = document.getElementsByClassName("addCourse");
+// const addStudentBtn = document.getElementsByClassName("addStudent");
+// const addAssignmentBtn = document.getElementsByClassName("addAssignment");
 
     // FORM TOGGLE
 const toggleTrainerForm = document.getElementById("toggleTrainer");
@@ -59,6 +59,92 @@ const student_CourseForm = document.forms.studentsPerCourse;
 const assignments_CourseForm = document.forms.assignmentsPerCourse;
 const bigForm = document.forms.assignmentsStudentCourse;
 let message = document.querySelectorAll(".message");
+
+// for Forms
+const toggleTrainer = toggleTrainerForm.addEventListener("click", function(){ 
+    return toggleForm(trainersForm);
+});
+const toggleStudent = toggleStudentForm.addEventListener("click", function(){ 
+    return toggleForm(studentsForm);
+});
+const toggleCourses = toggleCoursesform.addEventListener("click", function(){ 
+    return toggleForm(coursesForm);
+});
+const toggleAssignments = toggleAssingmentsform.addEventListener("click", function(){ 
+    return toggleForm(assignmentForm);
+});
+const toggleTrainer_Course = toggleTrainer_CourseForm.addEventListener("click", function(){ 
+    return toggleForm(trainer_CourseForm);
+});
+const toggleStudent_Course = toggleStudent_CourseForm.addEventListener("click", function(){ 
+    return toggleForm(student_CourseForm);
+});
+const toggleAssignments_Course = toggleAssignments_CourseForm.addEventListener("click", function(){ 
+    return toggleForm(assignments_CourseForm);
+});
+const toggleBig_Form = toggleBigForm.addEventListener("click", function(){ 
+    return toggleForm(bigForm);
+});
+
+
+// Toggle Function
+function toggleForm (form) {
+    if (form.style.display === "none" || form.style.display === "") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+    form.addEventListener("submit", function(x) {
+        x.preventDefault();
+    })
+}
+
+let listMaker = function () {
+    
+    
+}
+//Makes an array of objects
+const newCourse = courses.addEventListener("submit", function(){
+
+    function check() {
+        // if (courses.title.value === ""){
+        //     alert("Enter a Title");
+        // } else if (courses.type.value === ""){
+        //     alert("Enter course stream");
+        // } else if (courses.stream.value === ""){
+        //     alert("Enter course type");
+        // } else if (courses.start_date.value === ""){
+        //     alert("Enter start date");
+        // } else if (courses.end_date.value === ""){
+        //     alert("Enter end date");
+        // } else {}
+
+        let addCourse = function () {
+        let course = new Course(courses.title.value, courses.type.value, courses.stream.value, courses.start_date.value, courses.end_date.value, courseId);
+        courseArray.push(course);
+        courses.title.value = ""; // Can be function
+        courses.type.value = "";
+        courses.stream.value = "";
+        courses.start_date.value = "";
+        courses.end_date.value = "";
+        console.log(course);
+        }
+        addCourse();
+        newLine();
+        }
+
+
+    let newLine = function() {
+        thisEntry = courseId -1 ;
+        let ulData = document.querySelector(".ul.data");
+        let entry = document.createElement("li");
+        entry.append(`${courseId}. Course: ${courseArray[thisEntry].title}, ${courseArray[thisEntry].type}, ${courseArray[thisEntry].stream}. From ${courseArray[thisEntry].startDate} to ${courseArray[thisEntry].endDate} `);
+        ulData.appendChild(entry);
+        courseId++;
+
+    }
+    check();
+});
 
 
 //Our object constructors
@@ -105,106 +191,106 @@ class Trainer {
 
 
 // STUDENT INSTANCE
-addStudentBtn.addEventListener("click", function() { 
-    if (studentFirstName.value === ""){
-        return alert("Enter first name");
-    } else if (studentLastName.value === "") {
-        alert("Enter last name");
-    } else if (document.getElementById("dateOfBirth").value === ""){
-        alert("Enter an age");
-    } else if (document.getElementById("tuitionFees").value === "") {
-        alert("Enter the tuition");
-    } else { let addStudent = function () {   
-        let student = new Student(studentFirstName.value, studentLastName.value, dateOfBirth.value, tuitionFees.value);
-        studentArray.push(student);
-        studentId++;
-        studentFirstName.value = "";
-        studentLastName.value = "";
-        dateOfBirth.value = "";
-        tuitionFees.value = "";
-        console.log(studentArray);
-    }
-    addStudent();
-}
-});
+// addStudentBtn[0].addEventListener("click", function() { 
+//     if (studentFirstName.value === ""){
+//         return alert("Enter first name");
+//     } else if (studentLastName.value === "") {
+//         alert("Enter last name");
+//     } else if (document.getElementsByClassName("dateOfBirth").value === ""){
+//         alert("Enter an age");
+//     } else if (document.getElementsByClassName("tuitionFees").value === "") {
+//         alert("Enter the tuition");
+//     } else { let addStudent = function () {   
+//         let student = new Student(studentFirstName.value, studentLastName.value, dateOfBirth.value, tuitionFees.value);
+//         studentArray.push(student);
+//         studentId++;
+//         studentFirstName.value = "";
+//         studentLastName.value = "";
+//         dateOfBirth.value = "";
+//         tuitionFees.value = "";
+//         console.log(studentArray);
+//     }
+//     addStudent();
+// }
+// });
 
 
-// TRAINER INSTANCE
-addTrainerBtn.addEventListener("click", function() {
-    if (trainerFirstName.value === ""){
-        return alert("Enter first name");
-    } else if (trainerLastName.value === "") {
-        alert("Enter last name");
-    } else if (document.getElementById("trainerSubject").value === ""){
-        alert("Enter a Subject");
-    } else { let addTrainer = function () {
-        let trainer = new Trainer(trainerFirstName.value, trainerLastName.value, trainerSubject.value);
-        trainerArray.push(trainer);
-        trainerId++;
-        trainerFirstName.value = ""; // Can be function
-        trainerLastName.value = "";
-        trainerSubject.value = "";
-        console.log(trainerArray);
-    }
-    addTrainer();
-}
-});
+// // TRAINER INSTANCE
+// addTrainerBtn[0].addEventListener("click", function() {
+//     if (trainerFirstName.value === ""){
+//         return alert("Enter first name");
+//     } else if (trainerLastName.value === "") {
+//         alert("Enter last name");
+//     } else if (document.getElementsByClassName("trainerSubject").value === ""){
+//         alert("Enter a Subject");
+//     } else { let addTrainer = function () {
+//         let trainer = new Trainer(trainerFirstName.value, trainerLastName.value, trainerSubject.value);
+//         trainerArray.push(trainer);
+//         trainerId++;
+//         trainerFirstName.value = ""; // Can be function
+//         trainerLastName.value = "";
+//         trainerSubject.value = "";
+//         console.log(trainerArray);
+//     }
+//     addTrainer();
+// }
+// });
 
 
-// COURSE INSTANCE
-addCourseBtn.addEventListener("click", function() {
-    if (courseTitle.value === ""){
-        return alert("Enter a Title");
-    } else if (courseStream.value === ""){
-        alert("Enter course stream");
-    } else if (courseType.value === ""){
-        alert("Enter course type");
-    } else if (start_date.value === ""){
-        alert("Enter start date");
-    } else if (end_date.value === ""){
-        alert("Enter end date");
-    } else {let addCourse = function () {
-        let course = new Course(courseTitle.value, courseStream.value, courseType.value, start_date.value, end_date.value);
-        trainerArray.push(course);
-        courseId++;
-        courseTitle.value = ""; // Can be function
-        courseStream.value = "";
-        courseType.value = "";
-        start_date.value = "";
-        end_date.value = "";
-        console.log(course);
-    }
-    addCourse();
-    }
-});
+// // COURSE INSTANCE
+// addCourseBtn[0].addEventListener("click", function() {
+//     if (courseTitle.value === ""){
+//         return alert("Enter a Title");
+//     } else if (courseStream.value === ""){
+//         alert("Enter course stream");
+//     } else if (courseType.value === ""){
+//         alert("Enter course type");
+//     } else if (start_date.value === ""){
+//         alert("Enter start date");
+//     } else if (end_date.value === ""){
+//         alert("Enter end date");
+//     } else {let addCourse = function () {
+//         let course = new Course(courseTitle.value, courseStream.value, courseType.value, start_date.value, end_date.value);
+//         trainerArray.push(course);
+//         courseId++;
+//         courseTitle.value = ""; // Can be function
+//         courseStream.value = "";
+//         courseType.value = "";
+//         start_date.value = "";
+//         end_date.value = "";
+//         console.log(course);
+//     }
+//     addCourse();
+//     }
+// });
 
 
-// ASSIGNMENT INSTANCE
-addAssignmentBtn.addEventListener("click", function() {
-    if (assignmentTitle.value === ""){
-        return alert("Enter a Title");
-    } else if (assignmentDescription.value === "") {
-        alert("Enter a description");
-    } else if (assignmentSubDate.value === ""){
-        alert("Enter a Submission Date");
-    } else if (assignmentOralMark.value === ""){
-        alert("Enter an Oral Mark");
-    } else if (assignmentTotalMark.value === ""){
-        alert("Enter a Total Mark");
-    } else {let addAssignment = function () {
-        let assignment = new Assignment(assignmentTitle.value, assignmentDescription.value, assignmentSubDate.value, assignmentOralMark.value, assignmentTotalMark.value);
-        assignmentArray.push(assignment);
-        assignmentId++;
-        assignmentTitle.value = ""; // Can be function
-        assignmentDescription.value = "";
-        assignmentSubDate.value = "";
-        assignmentOralMark.value = "";
-        assignmentTotalMark.value = "";
-        console.log(assignment);
-    }
-    addAssignment();
-    }
-});
+// // ASSIGNMENT INSTANCE
+// addAssignmentBtn[0].addEventListener("click", function() {
+//     if (assignmentTitle.value === ""){
+//         return alert("Enter a Title");
+//     } else if (assignmentDescription.value === "") {
+//         alert("Enter a description");
+//     } else if (assignmentSubDate.value === ""){
+//         alert("Enter a Submission Date");
+//     } else if (assignmentOralMark.value === ""){
+//         alert("Enter an Oral Mark");
+//     } else if (assignmentTotalMark.value === ""){
+//         alert("Enter a Total Mark");
+//     } else {let addAssignment = function () {
+//         let assignment = new Assignment(assignmentTitle.value, assignmentDescription.value, assignmentSubDate.value, assignmentOralMark.value, assignmentTotalMark.value);
+//         assignmentArray.push(assignment);
+//         assignmentId++;
+//         assignmentTitle.value = ""; // Can be function
+//         assignmentDescription.value = "";
+//         assignmentSubDate.value = "";
+//         assignmentOralMark.value = "";
+//         assignmentTotalMark.value = "";
+//         console.log(assignment);
+//     }
+//     addAssignment();
+//     }
+// });
 
 
 
@@ -282,44 +368,7 @@ const studentLastListener = studentLastName.addEventListener('input', function()
     return nameValidate(studentLastName.value);
 });
 
-// for Forms
-const toggleTrainer = toggleTrainerForm.addEventListener("click", function(){ 
-    return toggleForm(trainersForm);
-});
-const toggleStudent = toggleStudentForm.addEventListener("click", function(){ 
-    return toggleForm(studentsForm);
-});
-const toggleCourses = toggleCoursesform.addEventListener("click", function(){ 
-    return toggleForm(coursesForm);
-});
-const toggleAssignments = toggleAssingmentsform.addEventListener("click", function(){ 
-    return toggleForm(assignmentForm);
-});
-const toggleTrainer_Course = toggleTrainer_CourseForm.addEventListener("click", function(){ 
-    return toggleForm(trainer_CourseForm);
-});
-const toggleStudent_Course = toggleStudent_CourseForm.addEventListener("click", function(){ 
-    return toggleForm(student_CourseForm);
-});
-const toggleAssignments_Course = toggleAssignments_CourseForm.addEventListener("click", function(){ 
-    return toggleForm(assignments_CourseForm);
-});
-const toggleBig_Form = toggleBigForm.addEventListener("click", function(){ 
-    return toggleForm(bigForm);
-});
 
-
-// Toggle Function
-function toggleForm (form) {
-    if (form.style.display === "none" || form.style.display === "") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
-    form.addEventListener("submit", function(x) {
-        x.preventDefault();
-    })
-}
 
 // GO TO TOP BUTTON
 let goTop = document.getElementById("goTop").addEventListener("click", function() {
